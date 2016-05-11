728a0f57f17fd8f7781520ef15bf72ff70586fa8
Huynh Khoa Vin <khoavin@gmail.com>
2016-05-11 17:11:09 +0700

    new

--- book.php
+++ book.php
@@ -1,8 +1,15 @@
 <?php
+$connect = mysqli_connect("localhost","root","","karaoke_booking");
+$user_id = 1;
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+$action = $_POST['action'];
+if (action == 'get_all_room_booked') {
+    $sql = "SELECT * FROM danh_sach_dat_phong". " WHERE TK_ID ='$user_id'";
+    $result = $connect->query($sql);
+    $mang = array();
+    while($data = mysqli_fetch_assoc($result)){
+        array_push($mang, $data);       
+  }
+  echo json_encode($mang);
+}
+?>
--- login.php
+++ login.php
@@ -1,4 +1,5 @@
 <?php
+$connect = mysqli_connect("localhost","root","","karaoke_booking");
 $action = $_POST["action"];
 if ($action=="login")
 {
@@ -6,11 +7,11 @@ if ($action=="login")
    $obj = json_decode($data);
    $user = $obj->{'username'};
    $pass = $obj->{'password'};
-   
-    $connect = mysqli_connect("localhost","root","","karaoke_booking");
+    
 	$sql = "SELECT * FROM tai_khoan". " WHERE TK_USER ='$user' and TK_PASS='$pass'";
 	$result = $connect->query($sql);
 	$row = mysqli_fetch_array($result,MYSQLI_NUM);
-	echo count($row[1]);
+	if(count($row[1])>=1)
+		echo "1";
 }
 ?>
